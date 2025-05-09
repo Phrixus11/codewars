003abad09c36f38b4c1fcd9c2382a1a7d3ee9274
YuryR <myaccount136@gmail.com>
2025-05-09 13:19:40 +0300

    Nuclear Missile Manager

--- 6kyu/nuclear-Missile-Manager.js
+++ 6kyu/nuclear-Missile-Manager.js
@@ -12,7 +12,7 @@
 // ✅Solution:
 
 // function launchAll(launchMissile) {
-//     for(var i = 0; i < 5; i++) {
+//     for(let i = 0; i < 5; i++) {
 //       setTimeout(function() {
 //         launchMissile(i);
 //       }, i * 1000);
